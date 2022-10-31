dcb87cf93c96a95cb8913ccc153eae8465a66db0
simonetta98 <simonatudisco@Air-di-simona.Home>
2022-10-31 12:33:48 +0100

    primoesercizioJS

---
+++ M1/W2/d1/JavaScript/index.js
@@ -0,0 +1,5 @@
+alert('Ciao!');
+alert('Sto studiando JavaScript!!!');
+alert('...')
+alert('...o almeno ci sto provando...')
+
---
+++ M1/W2/d1/JavaScript/naming.js
@@ -0,0 +1,17 @@
+//var 1 pet_preferito = 'gatto';
+//console.log(1 pet_preferito);
+
+/*var pet = 'gatto';
+var Pet = 'cane';*/
+var petPreferito = 'criceto';
+console.log(petPreferito);
+var PetPreferito = 'coniglio';
+console.log(PetPreferito);
+var $pet = 'giraffa';
+console.log($pet);
+var _pet = 'leone';
+console.log(_pet);
+var _pet2 = 'Pantera!';
+document.write(_pet2);
+
+//var 12 = 'numero';
