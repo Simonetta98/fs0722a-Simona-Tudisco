28e4a3cfd1b761733551e13871efc73c3e3b135e
simonetta98 <simonatudisco@Air-di-simona.Home>
2022-11-04 11:33:01 +0100

    calcolatrice

---
+++ M1/W2/d5/javascript/js.js
@@ -0,0 +1,28 @@
+function digita(elemento){                                     //permetto a tutto quello che digito di stare nel display
+    let tastoDigitato= elemento.value;
+    document.getElementById('display1').value += tastoDigitato;
+}
+function tot(){                                              //eseguo le operazioni che ora spuntano sul display
+    let display= document.getElementById('display1');
+    display.value= eval(display.value) ;                    //eval(anche se evil) mi permette di convertire il tutto in numeri (se no gli input rimangono stringhe)
+}
+function c(){                                                // tasto elimina tutto
+    let elimina= document.getElementById('display1');
+    elimina.value = '';
+}
+function ac(){                                               //tasto cancella ultima cifra (ho provato ma non funziona)
+    let canc= document.getElementById('display1').value;
+    canc= canc.slice(0,-1);
+    document.getElementById('display1').value= canc;
+    
+}
+function ics(){                                            //quando premo 'x' sulla calcolatrice il suo valore in display diventa '*'
+    let x= document.getElementById('display1');
+    x.value += '*';
+}
+function punto(){
+    let virgola= document.getElementById('display1');     //quando premo ',' sulla calcolatrice il suo valore in display diventa '.'
+    virgola.value += '.' ;
+}
+
+
