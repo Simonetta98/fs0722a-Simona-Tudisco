4d7763a613f49accc6340907975fb12d2eee7258
simonetta98 <simonatudisco@Air-di-simona.Home>
2022-12-09 13:34:45 +0100

    progettoSettimanale

--- M2/W3/d5/index.js
+++ M2/W3/d5/index.js
@@ -1,5 +1,5 @@
-///so che non c'era bisogno ma volevo allenarmi con gli oggetti
-//li costruisco solo con le proprietà che mi interessano 
+///so che non serviva, ma volevo allenarmi con gli oggetti:
+//li costruisco solo con le proprietà che mi interessa appaiano in display 
 class userProfile{
     constructor(firstName, lastName, email, img){
         this.firstName = firstName;
@@ -13,22 +13,27 @@ async function loadUsers() {
     const response = await fetch('users.json');
     var users = await response.json();
     console.log(users); 
+
 ///costruisco gli oggetti
+
+   /*
    var first = users[0] = new userProfile(users[0].firstName, users[0].lastName, users[0].email, users[0].profileURL);
     console.log(first)
    var second = users[1] = new userProfile(users[1].firstName, users[1].lastName, users[1].email, users[1].profileURL);
     console.log(second)
-    for(i=0; i<users.lenght; i++){
-        users[i]= new userProfile(users[i].firstName, users[i].lastName, users[i].email, users[i].profileURL);
-      console.log(users)
-    }
-
+    */ 
+   
+   //posso farlo con un ciclo:
+///uso un ciclo forEach
+    var u= [];
+    users.forEach((el,i) => {u[i]= new userProfile(users[i].firstName, users[i].lastName, users[i].email, users[i].profileURL)})
+    console.log(u)
 
 ///inserisco nell'html
     var title = document.querySelector('.card-body');
     var title2 = document.querySelector('.card-body2');
-    title.innerHTML = `<div><img class = 'rounded-circle bg-light p-3' src=${first.img}></img></div><h3>${first.firstName} ${first.lastName}</h3> <p class= 'text-warning'> ${first.email} </p>`;
-    title2.innerHTML = `<div><img class = 'rounded-circle bg-light p-3' src=${second.img}></img></div><h3>${second.firstName} ${second.lastName}</h3> <p class= 'text-warning'> ${second.email} </p>`;
+    title.innerHTML = `<div><img class = 'rounded-circle bg-light p-3' src=${u[0].img}></img></div><h3>${u[0].firstName} ${u[0].lastName}</h3> <p class= 'text-warning'> ${u[0].email} </p>`;
+    title2.innerHTML = `<div><img class = 'rounded-circle bg-light p-3' src=${u[1].img}></img></div><h3>${u[1].firstName} ${u[1].lastName}</h3> <p class= 'text-warning'> ${u[1].email} </p>`;
   }
 
   loadUsers();
