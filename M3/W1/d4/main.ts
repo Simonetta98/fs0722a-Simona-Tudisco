50426ada06f8e9a5bd726666d8eae8016c6af624
simonetta98 <simonatudisco@Air-di-simona.Home>
2022-12-22 20:00:04 +0100

    pSett

--- M3/W1/d4/main.ts
+++ M3/W1/d4/main.ts
@@ -81,10 +81,10 @@ console.log(m)
        </p>
        <hr>
        <p class="text-center">
-       Prezzo: <span class="text-decoration-line-through">${Math.round(obj.prezzoivainclusa)} €</span> <span class="ms-4 fs-4 fw-semibold">${obj.getacquistocapo()} €</span>
+       Prezzo: <span class="text-decoration-line-through">${Math.round(obj.prezzoivainclusa)} €</span> <span class="ms-4 fs-4 fw-semibold ">${obj.getacquistocapo()} €</span>
        </p>
        <div class="container text-center">
-      <a id="${obj.id, obj.img, obj.capo}" href="#" class="btn  shadow bg-body rounded">Mettilo in busta <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag mb-1 ms-1" viewBox="0 0 16 16">
+      <a id="${obj.capo}   , ${obj.collezione} ${obj.getacquistocapo()}" href="#" class="btn  shadow bg-body rounded ">Mettilo in busta <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag mb-1 ms-1" viewBox="0 0 16 16">
       <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
     </svg></a>
     </div>
@@ -96,35 +96,21 @@ var buttons = Array.from(document.querySelectorAll('.btn'))
 console.log(buttons)
 buttons.forEach(function(el) {
 el.addEventListener('click', function() {
-   sessionStorage.setItem('id', el.id);
-  
    let container2 = (<HTMLDivElement>document.getElementById('cart'));
 
    container2.innerHTML += ` 
    <div class="card mb-4 me-4 boh" style="width: 18rem;">
    <div class="card-body">
    <p class="d-none codprod">${el.id}</p> 
-     <h5 id="nome" class="card-title fw-semibold">${el.id}</h5> 
-     <p class="card-text">Questo capo appartiene alla collezione <span>${obj.collezione}</span>.
+     <h5 id="nome" class="card-title fw-semibold">${el.id.slice(0, 8)} <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-bag-heart ms-2 mb-1" viewBox="0 0 16 16">
+     <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+   </svg></h5> 
+     <p class="card-text">Collezione: <span>${el.id.slice(0, -2)}</span>.
      </p>
-     <p>
-     Affrettati, ne rimangolo solo <span>${obj.quantita}</span> in <span>${obj.disponibile}</span>!
-      </p>
-      <p>
-      Colore: ${obj.colore} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-palette2 mb-1" viewBox="0 0 16 16">
-      <path d="M0 .5A.5.5 0 0 1 .5 0h5a.5.5 0 0 1 .5.5v5.277l4.147-4.131a.5.5 0 0 1 .707 0l3.535 3.536a.5.5 0 0 1 0 .708L10.261 10H15.5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5H3a2.99 2.99 0 0 1-2.121-.879A2.99 2.99 0 0 1 0 13.044m6-.21 7.328-7.3-2.829-2.828L6 7.188v5.647zM4.5 13a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0zM15 15v-4H9.258l-4.015 4H15zM0 .5v12.495V.5z"/>
-      <path d="M0 12.995V13a3.07 3.07 0 0 0 0-.005z"/>
-    </svg>
-      </p>
       <hr>
       <p class="text-center">
-      Prezzo: <span class="text-decoration-line-through">${Math.round(obj.prezzoivainclusa)} €</span> <span class="ms-4 fs-4 fw-semibold">${obj.getacquistocapo()} €</span>
+      Prezzo: <span class="ms-4 fs-4 fw-semibold">${el.id.slice(-2)}€</span>
       </p>
-      <div class="container text-center">
-     <a id="${obj.id}" href="#" class="btn  shadow bg-body rounded">Mettilo in busta <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag mb-1 ms-1" viewBox="0 0 16 16">
-     <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
-   </svg></a>
-   </div>
    </div>
  </div>`
    });
